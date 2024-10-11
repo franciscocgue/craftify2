5620446d64a86d91cf964018b761c1c478e4de0f
franciscocgue <franciscocguerrero@gmail.com>
2024-10-11 22:31:48 +0200

    test messages updated

--- frontend/src/utils/css-utils.test.ts
+++ frontend/src/utils/css-utils.test.ts
@@ -2,8 +2,10 @@ import { describe, expect, it } from 'vitest';
 import { isValidCssLength } from './css-utils';
 
 describe('css-utils', () => {
-    it('isValidCssLength(25px) as true', () => { expect(isValidCssLength('25px')).toBe(true) })
-    it('isValidCssLength(25) as false', () => { expect(isValidCssLength('25')).toBe(false) })
-    it('isValidCssLength(20%) as true', () => { expect(isValidCssLength('20%')).toBe(true) })
-    it('isValidCssLength(30vw) as false (not supported)', () => { expect(isValidCssLength('30vw')).toBe(false) })
+    describe('isValidCssLength', () => {
+        it('25px as valid', () => { expect(isValidCssLength('25px')).toBe(true) })
+        it('25 as invalid', () => { expect(isValidCssLength('25')).toBe(false) })
+        it('20% as valid', () => { expect(isValidCssLength('20%')).toBe(true) })
+        it('30vw as invalid (not supported)', () => { expect(isValidCssLength('30vw')).toBe(false) })
+    })
 })
